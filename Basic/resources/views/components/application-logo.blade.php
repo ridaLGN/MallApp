ff9d11a408655fd8772ef577e0da9efa3611033e
unknown <lagnaoui.rida.iir@gmail.com>
2023-11-03 15:34:34 +0100

    first commit

---
+++ Basic/resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+ <img src="  {{ asset('logo/trainingedge.png')  }} "  width="100px">
---
+++ Basic/resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>contact page </title>
+</head>
+<body>
+	<h2> this is contact page  </h2>
+	<a href=" {{ route ('about.page')  }}"> about </a>
+
+</body>
+</html> 
